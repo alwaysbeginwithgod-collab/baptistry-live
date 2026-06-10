9b80c8d2cf101788962cee55994fad1c36a0ea8c
alwaysbeginwithgod-collab <always.begin.with.god@yahoo.com>
2026-06-10 08:12:11 +0800

    completely remove convex, clean layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,6 @@ import { ThemeProvider } from './context/ThemeContext'
 import { ClerkProvider } from '@clerk/nextjs'
 import Script from 'next/script';
 import { Analytics } from '@vercel/analytics/react';
-import { ConvexProvider, ConvexReactClient } from "convex/react";
-import { ChatProvider } from './context/ChatContext';
-
-// Create the Convex client
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export default function RootLayout({
   children,
@@ -17,29 +12,24 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ConvexProvider client={convex}>
-      <ClerkProvider>
-        <ThemeProvider>
-          <ChatProvider>
-            <html lang="en">
-              <head>
-                <title>BAPTISTRY - KJV Bible Teaching</title>
-                <meta name="description" content="Biblical teaching from the King James Version" />
-                <link rel="icon" href="/baptistry-logo.png" />
-              </head>
-              <body>
-                {/* PayPal SDK - Loads once */}
-                <Script 
-                  src="https://www.paypal.com/sdk/js?client-id=BAAdMBwnGSRahrsJy5-2A9wIqaqgZOjVM0jLL0kauYAny-raGaAZh8Xozwou1oAL4n07BEVdl2ex6WUdVM&components=hosted-buttons&disable-funding=venmo&currency=USD"
-                  strategy="afterInteractive"
-                />
-                {children}
-                <Analytics />
-              </body>
-            </html>
-          </ChatProvider>
-        </ThemeProvider>
-      </ClerkProvider>
-    </ConvexProvider>
+    <ClerkProvider>
+      <ThemeProvider>
+        <html lang="en">
+          <head>
+            <title>BAPTISTRY - KJV Bible Teaching</title>
+            <meta name="description" content="Biblical teaching from the King James Version" />
+            <link rel="icon" href="/baptistry-logo.png" />
+          </head>
+          <body>
+            <Script 
+              src="https://www.paypal.com/sdk/js?client-id=BAAdMBwnGSRahrsJy5-2A9wIqaqgZOjVM0jLL0kauYAny-raGaAZh8Xozwou1oAL4n07BEVdl2ex6WUdVM&components=hosted-buttons&disable-funding=venmo&currency=USD"
+              strategy="afterInteractive"
+            />
+            {children}
+            <Analytics />
+          </body>
+        </html>
+      </ThemeProvider>
+    </ClerkProvider>
   );
 }
