27dc1e6547d6316d2923c0c51a3a40a9fad16169
alwaysbeginwithgod-collab <always.begin.with.god@yahoo.com>
2026-06-11 06:11:03 +0800

    speed up typing response to 5ms

--- app/MainContent.tsx
+++ app/MainContent.tsx
@@ -339,7 +339,7 @@ export default function MainContent() {
           return;
         }
         setStreamingText(fullResponse.substring(0, i));
-        await new Promise(resolve => setTimeout(resolve, 8));
+        await new Promise(resolve => setTimeout(resolve, 5));
       }
       
       // Add the complete message to the chat
