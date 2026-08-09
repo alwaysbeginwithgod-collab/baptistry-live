bf93b57105e305c2259f1967b0f163048fcd3070
Always Begin With God <always.begin.with.god@gmail.com>
2026-08-09 23:47:49 +0800

    Fix React import for React.Children in MessageBubble

--- app/components/MessageBubble.tsx
+++ app/components/MessageBubble.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { findVerseReferences, formatVerseReference, getVerseText } from '../lib/verseUtils';
