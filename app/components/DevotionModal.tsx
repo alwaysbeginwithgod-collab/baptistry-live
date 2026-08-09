9887369d05ccc022d3423c4c27d0e9c8a838ac91
Always Begin With God <always.begin.with.god@gmail.com>
2026-08-10 07:32:48 +0800

    message

--- app/components/DevotionModal.tsx
+++ app/components/DevotionModal.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect, useMemo, useRef } from 'react';
-import { devotions, Devotion } from '../admin/dailyDevotions';
+import { DEVOTIONS } from '../admin/dailyDevotions';
 
 interface DevotionModalProps {
   isOpen: boolean;
