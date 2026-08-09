5e8b513a6c98318db2c6a8da65e6679ef14a0b3c
Always Begin With God <always.begin.with.god@gmail.com>
2026-08-10 07:54:22 +0800

    Complete admin folder restructure with all content files

--- app/components/DevotionModal.tsx
+++ app/components/DevotionModal.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect, useMemo, useRef } from 'react';
-import { DEVOTIONS } from '../admin/dailyDevotions';
+import { devotions } from '../admin/dailyDevotions';
 
 interface DevotionModalProps {
   isOpen: boolean;
